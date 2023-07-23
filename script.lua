2b92af9b4df70c4e7aaa79f1b28f0dc4ce76119d
imsmartbutdumb <92808055+imsmartbutdumb@users.noreply.github.com>
2023-07-23 00:10:43 -0500

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,412 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+local TextLabel_2 = Instance.new("TextLabel")
+local TextButton = Instance.new("TextButton")
+local Grapher = Instance.new("TextButton")
+local Sped = Instance.new("TextButton")
+
+--Properties:
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ResetOnSpawn = false
+
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(255, 85, 0)
+Frame.Position = UDim2.new(0.37599501, 0, 0.404970765, 0)
+Frame.Size = UDim2.new(0, 216, 0, 212)
+Frame.Active = true
+Frame.Draggable = true
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(170, 0, 0)
+TextLabel.Size = UDim2.new(0, 216, 0, 37)
+TextLabel.Font = Enum.Font.Cartoon
+TextLabel.Text = "HAHA sploiters ff2 HACK"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextSize = 20.000
+
+TextLabel_2.Parent = Frame
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 170, 255)
+TextLabel_2.Position = UDim2.new(0, 0, 0.864253402, 0)
+TextLabel_2.Size = UDim2.new(0, 216, 0, 30)
+TextLabel_2.Font = Enum.Font.SourceSans
+TextLabel_2.Text = "BY Pigu4321"
+TextLabel_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.TextSize = 14.000
+
+TextButton.Parent = Frame
+TextButton.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
+TextButton.Position = UDim2.new(0.0370370373, 0, 0.211167425, 0)
+TextButton.Size = UDim2.new(0, 200, 0, 43)
+TextButton.Font = Enum.Font.SourceSans
+TextButton.Text = "USE MAGSCRIPT"
+TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton.TextSize = 35.000
+
+Grapher.Name = "Grapher"
+Grapher.Parent = Frame
+Grapher.BackgroundColor3 = Color3.fromRGB(0, 0, 127)
+Grapher.Position = UDim2.new(0.0367535949, 0, 0.448125064, 0)
+Grapher.Size = UDim2.new(0, 200, 0, 41)
+Grapher.Font = Enum.Font.Cartoon
+Grapher.Text = "Ball Predictions"
+Grapher.TextColor3 = Color3.fromRGB(255, 170, 0)
+Grapher.TextSize = 33.000
+
+Sped.Name = "Sped"
+Sped.Parent = Frame
+Sped.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Sped.Position = UDim2.new(0.0359533802, 0, 0.671438813, 0)
+Sped.Size = UDim2.new(0, 200, 0, 34)
+Sped.Font = Enum.Font.Cartoon
+Sped.Text = "SPED (G)"
+Sped.TextColor3 = Color3.fromRGB(0, 0, 0)
+Sped.TextSize = 45.000
+
+-- Scripts:
+
+local function BRQX_fake_script() -- TextLabel.LocalScript 
+	local script = Instance.new('LocalScript', TextLabel)
+
+	while wait () do
+		script.Parent.BackgroundColor3 = Color3.fromRGB(255, 8, 4)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(255, 143, 7)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(235, 255, 11)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(75, 255, 10)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(8, 148, 255)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(255, 0, 4)
+		wait(0.01)
+	end
+end
+coroutine.wrap(BRQX_fake_script)()
+local function FXTAMUH_fake_script() -- TextLabel_2.LocalScript 
+	local script = Instance.new('LocalScript', TextLabel_2)
+
+	while wait () do
+		script.Parent.BackgroundColor3 = Color3.fromRGB(255, 8, 4)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(255, 143, 7)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(235, 255, 11)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(75, 255, 10)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(8, 148, 255)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(255, 0, 4)
+		wait(0.01)
+	end
+end
+coroutine.wrap(FXTAMUH_fake_script)()
+local function YXOML_fake_script() -- TextButton.LocalScript 
+	local script = Instance.new('LocalScript', TextButton)
+
+	while wait () do
+		script.Parent.BackgroundColor3 = Color3.fromRGB(255, 8, 4)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(255, 143, 7)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(235, 255, 11)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(75, 255, 10)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(8, 148, 255)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(255, 0, 4)
+		wait(0.01)
+	end
+end
+coroutine.wrap(YXOML_fake_script)()
+local function NPJZSV_fake_script() -- Grapher.LocalScript 
+	local script = Instance.new('LocalScript', Grapher)
+
+	while wait () do
+		script.Parent.BackgroundColor3 = Color3.fromRGB(255, 8, 4)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(255, 143, 7)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(235, 255, 11)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(75, 255, 10)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(8, 148, 255)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(255, 0, 4)
+		wait(0.01)
+	end
+end
+coroutine.wrap(NPJZSV_fake_script)()
+local function KFBXOV_fake_script() -- Sped.LocalScript 
+	local script = Instance.new('LocalScript', Sped)
+
+	while wait () do
+		script.Parent.BackgroundColor3 = Color3.fromRGB(255, 8, 4)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(255, 143, 7)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(235, 255, 11)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(75, 255, 10)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(8, 148, 255)
+		wait(0.01)
+		script.Parent.BackgroundColor3 = Color3.fromRGB(255, 0, 4)
+		wait(0.01)
+	end
+end
+coroutine.wrap(KFBXOV_fake_script)()
+
+Grapher.MouseButton1Down:connect(function()
+	local Grapher = {}
+
+	Grapher.Marker = Instance.new("Part")
+	Grapher.Marker.Anchored = true
+	Grapher.Marker.Transparency = .8
+	Grapher.Marker.Color = Color3.fromRGB(255, 42, 0)
+	Grapher.Marker.CanCollide = false
+	Grapher.Marker.Name = "Marker"
+
+	Grapher.Params = RaycastParams.new()
+	Grapher.Params.IgnoreWater = true 
+	Grapher.Params.FilterType = Enum.RaycastFilterType.Whitelist
+
+	Grapher.CastStep = 3 / 60
+
+	Grapher.LastSavedPower = 60
+
+	function Grapher:GetCollidables()
+		local Collidables = {}
+
+		for _, BasePart in ipairs(workspace:GetDescendants()) do
+			if BasePart:IsA("BasePart") and BasePart.CanCollide == true then
+				table.insert(Collidables, BasePart)
+			end
+		end
+		return Collidables
+	end
+
+	function Grapher:WipeMarkers()
+		for i,v in pairs(workspace:GetChildren()) do
+			if v.Name == "Marker" then v:Destroy() end
+		end
+	end
+
+	function Grapher:GetLanding(origin, velocity, c)
+		local Elapsed = 0
+		local LastPos = origin
+
+		self.Params.FilterDescendantsInstances = self:GetCollidables()
+
+		while true do
+			Elapsed += Grapher.CastStep
+
+			local nPos = origin + velocity * Elapsed - Vector3.new(0, .5 * 28 * Elapsed ^ 2, 0)
+
+			local Marker = self.Marker:Clone(); Marker.Parent = workspace; Marker.Position = nPos
+			if c and c.Parent ~= workspace or c and not c:FindFirstChildOfClass("BodyForce") then
+				self:WipeMarkers()
+				break
+			end
+
+			task.wait()
+		end
+	end
+
+	do
+		workspace.ChildAdded:Connect(function(child)
+			if child.Name == "Football" and child:IsA("BasePart") then
+				local tempCon; tempCon = child:GetPropertyChangedSignal("Velocity"):Connect(function()
+					Grapher:GetLanding(child.Position, child.Velocity, child)
+					tempCon:Disconnect()
+				end)
+			end
+		end)     
+	end
+
+	return Grapher 
+
+end)
+
+Sped.MouseButton1Down:connect(function()
+	local s_rs = game:GetService("RunService")
+	local l_plr = game:GetService("Players").LocalPlayer
+
+	local l_humrp = l_plr.Character and l_plr.Character:FindFirstChild("HumanoidRootPart")
+	local l_hum = l_plr.Character and l_plr.Character:FindFirstChild("Humanoid")
+
+	local resp_con = l_plr.CharacterAdded:Connect(function(c) 
+		l_humrp = c:WaitForChild("HumanoidRootPart",3)
+		l_hum = c:WaitForChild("Humanoid",3)
+	end)
+
+	local function dnec(signal) 
+		local s = {}
+		for _, con in ipairs(getconnections(signal)) do 
+			local func = con.Function
+			if (func and islclosure(func)) then
+				if (not is_synapse_function(func)) then 
+					s[#s+1] = con
+					con:Disable() 
+				end
+			end
+		end
+		return s
+	end
+
+	local speed_amnt = 5
+	local is_speed_enabled = false
+
+	local function toggle_speed()
+		is_speed_enabled = not is_speed_enabled
+		if is_speed_enabled then
+			do
+				local a,b
+
+				a = dnec(l_humrp.Changed)
+				b = dnec(l_humrp:GetPropertyChangedSignal("CFrame"))
+
+				s_rs:BindToRenderStep("speed",2000,function(dt)
+					l_humrp.CFrame += l_hum.MoveDirection*dt*5*speed_amnt
+				end)
+			end
+		else
+			s_rs:UnbindFromRenderStep("speed")
+		end
+	end
+
+	game:GetService("UserInputService").InputBegan:Connect(function(input)
+		if input.KeyCode == Enum.KeyCode.G then
+			toggle_speed()
+		end
+	end)
+
+	resp_con:Disconnect()
+
+end)
+
+TextButton.MouseButton1Down:connect(function()
+	local function callback(Text)
+		if Text == "Button1 text" then
+			print ("Answer")
+		elseif Text == ("Button2 text") then
+			print ("Answer2")
+		end
+	end
+
+	local NotificationBindable = Instance.new("BindableFunction")
+	NotificationBindable.OnInvoke = callback
+	--
+	game.StarterGui:SetCore("SendNotification",  {
+		Title = "MS ON";
+		Text = "your hacking now :P";
+		Icon = "";
+		Duration = 5;
+		Callback = NotificationBindable;
+	})
+
+	local lp = game:GetService("Players").LocalPlayer
+	local stepped = game:GetService("RunService").Stepped
+	local user = game:GetService("UserInputService")
+	local gui = game:GetService("GuiService")
+
+	local maxdistance = 25 --Max distance you can catch a football from, server caps it at some point anyways
+	local autoclickoffset = nil --How far off from the max distance to click, nil for no auto
+
+	local escmenu = false
+
+	autoclickoffset = autoclickoffset and autoclickoffset + maxdistance
+
+	local infinite = math.huge
+	local workspace = workspace
+	local firetouchinterest = firetouchinterest
+	local FindFirstChild = game.FindFirstChild
+	local FindFirstChildOfClass = game.FindFirstChildOfClass
+	local task = task
+	local connect = stepped.Connect
+	local waitfor = stepped.Wait
+	local keypress = keypress
+	local keyrelease = keyrelease
+	local gettextbox = user.GetFocusedTextBox
+	local pairs = pairs
+
+	local catchparts = {
+		"CatchRight",
+		"CatchLeft"
+	}
+
+	local function resolveShortest(ball, charac)
+		local shortest, chosen = infinite, nil
+
+		for i, name in pairs(catchparts) do
+			local part = FindFirstChild(charac, name)
+			local dist = part and (part.Position - ball.Position).Magnitude or infinite
+
+			if dist < shortest then
+				shortest = dist
+				chosen = part
+			end
+		end
+
+		return shortest, chosen
+	end
+
+	local function WaitForChildOfClass(inst, child, maxtime)
+		local timer = 0
+		maxtime = maxtime or infinite
+
+		local found = FindFirstChildOfClass(inst, child)
+		while not found and timer < maxtime do
+			timer = timer + task.wait()
+			found = FindFirstChildOfClass(inst, child)
+
+			if found then
+				return found
+			end
+		end
+
+		return found
+	end
+
+	connect(gui.MenuOpened, function()
+		escmenu = true
+	end)
+
+	connect(gui.MenuClosed, function()
+		escmenu = false
+	end)
+
+	connect(workspace.ChildAdded, function(ball)
+		if ball.Name == "Football" and WaitForChildOfClass(ball, "TouchTransmitter", 3) then
+			local charac = lp.Character
+
+			while ball.Parent == workspace and charac do
+				local dist, part = resolveShortest(ball, charac)
+
+				if part then
+					if autoclickoffset and dist <= autoclickoffset and not (gettextbox(user) or escmenu) then
+						keypress(0x43) --C key, not mouse click cus that can do gay shit like close out roblox
+						keyrelease(0x43)
+					end
+
+					if dist <= maxdistance then
+						firetouchinterest(part, ball, 1)
+						firetouchinterest(part, ball, 0)
+					end
+				end
+
+				waitfor(stepped)
+			end
+		end
+	end)
+end)
